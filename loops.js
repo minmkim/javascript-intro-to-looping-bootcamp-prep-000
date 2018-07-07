233eb7ba143363c6acda0420d1c1ec2acef84daa
Min Kim <minmkim@gmail.com>
2018-07-07 04:27:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
   if (i == 0) {
-    return "I am 1 strange loop."
+    array.push("I am 1 strange loop.")
   } else {
-    return `I am ${i} strange loops.`
+    array.push(`I am ${i} strange loops.`)
   }
   }
+  return array
 }
 
 function whileLoop(number) {
