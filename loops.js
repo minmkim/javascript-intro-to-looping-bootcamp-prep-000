02948643c05ffebc138e597a49c8803aa4514a36
Min Kim <minmkim@gmail.com>
2018-07-07 04:24:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,9 +3,22 @@ function forLoop() {
   if (i == 0) {
     return "I am 1 strange loop."
   } else {
-    return `I am ${i} strange loops."
+    return `I am ${i} strange loops.`
   }
   }
 }
 
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(parseInt(number--, 10))
+  }
+}
 
+function doWhileLoop(array) {
+  do {
+    array.shift()
+  } while array.length > 0 && maybeTrue() {
+    array.shift()
+  }
+  return array
+}
